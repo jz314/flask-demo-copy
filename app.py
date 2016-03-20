acaabe954347ee2322887f7956be5c06c0aa9f0f
jz314 <j.zangh@gmail.com>
2016-03-20 07:39:35 -0400

    app

--- app.py
+++ app.py
@@ -2,13 +2,25 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+app.vars={}
+
 @app.route('/')
 def main():
   return redirect('/index')
 
-@app.route('/index')
+@app.route('/index',methods=['GET','POST'])
 def index():
-  return render_template('index.html')
+	if request.method=='GET':
+ 		return render_template('index.html')
+	else: #request was a POST
+ 		app.vars['ticker']=request.form['ticker']
+
+ 		f=open('%s.txt'%(app.vars['ticker']),'w')
+ 		f.write('Ticker: %s\n'%(app.vars['ticker']))
+ 		f.close()
+
+ 	return 'request.method was not a GET!'
 
 if __name__ == '__main__':
-  app.run(port=33507)
+	app.run(host='0.0.0.0')
+#  app.run(port=33507)
