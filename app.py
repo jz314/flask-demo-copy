dfae577575047ad2f10be28431358c03c30c8a87
jz314 <j.zangh@gmail.com>
2016-03-21 02:45:37 -0400

    updated app.py and bokeh package, which somehow solved the jinja2 bug

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,url_for,send_from_directory
+from bokeh.embed import components
+from helperFunctions import *
 
-app = Flask(__name__)
+#http://stackoverflow.com/questions/20646822/how-to-serve-static-files-in-flask
+app = Flask(__name__,static_folder='static')
 
 app.vars={}
 
@@ -14,13 +17,28 @@ def index():
  		return render_template('index.html')
 	else: #request was a POST
  		app.vars['ticker']=request.form['ticker']
+ 		requestedjson=requestjson_v2(app.vars['ticker'],base_url,requestParams)
 
- 		f=open('%s.txt'%(app.vars['ticker']),'w')
- 		f.write('Ticker: %s\n'%(app.vars['ticker']))
- 		f.close()
+ 		#check if request resulted in valid data
+		if requestedjson.keys()==[u'quandl_error']:
+			#route to error_page
+			return render_template('error.html')
 
- 	return 'request.method was not a GET!'
+		else:
+			df=v2_json_to_dataframe(str(app.vars['ticker']),requestedjson)#put into pandas df
+			makeplot(df)
+#			return redirect('/static_from_root')
+#			return redirect(url_for('static',filename='plot.html'))
+			return render_template('plot.html')
+
+@app.route('/static_from_root')
+def static_from_root(): 
+	return render_template('plot.html')
+#	return render_template('index.html')
+#	return 'I am Chug' 
+#send_from_directory(app.static_folder,request.path[1:])
+
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0',debug=True)
 #  app.run(port=33507)
---
+++ helperFunctions.py
@@ -0,0 +1,69 @@
+import matplotlib
+matplotlib.use('Agg')
+#helper functions
+
+import requests
+from requests import get
+import json
+import simplejson as json
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.pylab
+from bokeh import mpl
+from bokeh.plotting import output_file, show,save
+from bokeh.plotting import figure
+from bokeh.embed import components
+import time
+import datetime
+import numpy as np
+#####requesting data
+
+
+currentDay=time.strftime("%Y-%m-%d")
+requestParams={'start_date':'2016-02-18','end_date':currentDay,'api_key':'dLzgUM9W7Aqpmy3RRh4A','order':'asc',\
+               'format':'json','column_index':'3','column_index':'4'\
+              }
+
+base_url='https://www.quandl.com/api/v3/datasets/WIKI/'
+def requestjson_v2(ticker,base_url,requestParams):
+    return requests.get(base_url+ticker,params=requestParams).json()
+
+
+def checkjson(requestedjson):
+	if requestedjson.keys()==[u'quandl_error']:
+		print 'ERROR. Most likely the ticker you entered is not in the WIKI stock set.'
+#print requestjson_v2('AAPL',base_url,requestParams)
+
+def v2_json_to_dataframe(ticker,jsonInput):
+    df=pd.read_json(json.dumps(jsonInput)).transpose()[['data']]
+    ts=df['data'].apply(pd.Series).transpose()
+    ts['date']=ts['dataset'].str[0]
+    ts[ticker]=ts['dataset'].str[1]
+    del ts['dataset']
+    return ts 
+
+def makeplot(dataframe):
+	plt.figure(figsize=(11,8))
+	sns.set(style="darkgrid")
+	dataframe.plot()#df plot function
+#	ax.legend(bbox_to_anchor=(1,1),bbox_transform=plt.gcf().transFigure)    
+	plt.legend(fontsize=20,bbox_to_anchor=(1.05,1),loc=2)
+	plt.ylabel('Closing Price',fontsize=15)
+	plt.xlabel('Day',fontsize=15)
+#	fig.savefig('AAPL.png')
+#	output_file("templates/plot.html",title="test")
+	output_file("templates/plot.html",title="closingPrices")
+#	show(mpl.to_bokeh())
+	save(mpl.to_bokeh())
+#	plot=mpl.to_bokeh()
+#	return plot
+
+
+#plot=figure()	
+
+#requestedjson=requestjson_v2('AAPL',base_url,requestParams)
+#checkjson(requestedjson)
+#df=v2_json_to_dataframe('AAPL',requestedjson)
+#df.to_csv("df.out.test")
+#makeplot(df)
